cc6a058d096d9da865013d258c6fdfb97efc49dd
Matthew Waskin <matthew@waskin.com>
2023-07-27 15:21:25 -0400

    manually unsubscribe from URL params to demonstrate how subscriptions should be handled when components are destroyed, though Angular does this automatically

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  // see comment on ngOnDestroy hook
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -18,9 +21,14 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name'],
     };
     /* This way uses the params Observable to listen to changes to the params and reacts to the change by reassigning the user.id and user.name, triggering a rerender to update the data bound in the template */
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'];
       this.user.name = params['name'];
     });
+  }
+
+  // the code in this hook is not necessary bc Angular handles this behind the scenes; this manually stop listening to changes in the route params when the component is removed from the DOM
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
