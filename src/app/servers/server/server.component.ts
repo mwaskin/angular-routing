df8e0533672eef82291557b714e7474c73960873
Matthew Waskin <matthew@waskin.com>
2023-07-28 19:15:43 -0400

    learn how to use queryParams and how to preserve/merge them when navigating

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,17 +10,25 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
-  serverIdToGet: number;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
-      this.serverIdToGet = +params['id'];
+      this.server = this.serversService.getServer(+params['id']);
     });
-    this.server = this.serversService.getServer(this.serverIdToGet);
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
   }
 }
